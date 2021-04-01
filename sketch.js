5eac80c8daf71dfc15bee2c9d05074361e4cc9aa
tanishqp11 <78473065+tanishqp11@users.noreply.github.com>
2021-04-01 21:01:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,67 @@
+var starImg, fairyImg, bgImg;
+var fairy , fairyVoice;
+var star, starBody;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload(){
+	starImg = loadImage("star.png");
+	fairyImg = loadAnimation("fairy1.png","fairy2.png");
+	bgImg = loadImage("starNight.png");
+	fairyVoice = loadSound("JoyMusic.mp3");
+}
+
+function setup() {
+	createCanvas(800, 750);
+
+	fairyVoice.play();
+
+	fairy = createSprite(130, 520);
+	fairy.addAnimation("fairyflying",fairyImg);  
+	fairy.scale = 0.25;
+
+	star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
+
+	engine = Engine.create();
+	world = engine.world;
+
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:false});
+	World.add(world, starBody);
+	
+	Engine.run(engine);
+
+}
+
+
+function draw() {
+  background(bgImg);
+  
+  keyPressed();
+  
+  if(star.position.y > 470){
+	star.y = 470;
+	fairy.y = 500;
+	fairy.x = star.x - 140;
+  }
+
+  drawSprites();
+
+}
+
+function keyPressed() {
+	//write code here
+	if(keyDown(LEFT_ARROW)){
+		fairy.x -= 5;
+	} else if(keyDown(RIGHT_ARROW)){
+		fairy.x += 5;
+	} else if(keyDown(DOWN_ARROW)){
+		star.velocityY = 15;
+	}
+}
+
+
